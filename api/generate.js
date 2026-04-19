2b5f1b16e4b5a2fcb29ab84d2549df3759dff1d5
Ayan Karmakar <karmakarayan37@gmail.com>
2026-04-19 16:28:05 +0530

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -16,6 +16,16 @@ export default async function handler(req, res) {
       process.env.GEMINI_KEY_3,
       process.env.GEMINI_KEY_4,
       process.env.GEMINI_KEY_5,
+      process.env.GEMINI_KEY_6,
+      process.env.GEMINI_KEY_7,
+      process.env.GEMINI_KEY_8,
+      process.env.GEMINI_KEY_9,
+      process.env.GEMINI_KEY_10,
+      process.env.GEMINI_KEY_11,
+      process.env.GEMINI_KEY_12,
+      process.env.GEMINI_KEY_13,
+      process.env.GEMINI_KEY_14,
+      process.env.GEMINI_KEY_15,
       process.env.GEMINI_API_KEY, // system fallback
     ].filter(Boolean);
 
